03cf5d69f3104e7aebfa666535edc7b10f3571bd
brianmanderson <markba122@gmail.com>
2023-10-05 15:56:05 -0400

    rename, add Json file support

--- MakeRaystationDataStructure_CSharp/MainWindow.xaml.cs
+++ MakeRaystationDataStructure_CSharp/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.Json;
+using DataBaseStructure.Base;
 
 
 namespace MakeRaystationDataStructure_CSharp
